2f946ec5653807fc757d3f84db5fd95d9a91f3e7
tanpenggood <tanpengswpu@163.com>
2023-09-20 00:56:36 +0800

    single line comment

--- python/basics/single_line_comment/example3.py
+++ python/basics/single_line_comment/example3.py
@@ -1,1 +1,1 @@
-# Create DynamoDB table for users using email as primary key and date created as sort key
+# Function to create DynamoDB table for users using email as primary key and date created as sort key
