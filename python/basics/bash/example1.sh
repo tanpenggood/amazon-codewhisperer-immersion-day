60daaeeb21651a1f6007b225b74c728d3911357a
tanpenggood <tanpengswpu@163.com>
2023-09-19 23:39:15 +0800

    rm python/basics/bash

--- python/basics/bash/example1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Remove all unused containers
-
-# Remove all unused images
-
-# Remove all unused volumes
--- python/basics/bash/example2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-# Stop all EC2 instances tagged environment=dev
