147eb7c39866e692c6647fe497200d2d2e976aee
tanpenggood <tanpengswpu@163.com>
2023-09-22 14:24:25 +0800

    algorithms

--- python/basics/06_algorithms/example1.py
+++ python/basics/06_algorithms/example1.py
@@ -1,15 +1,1 @@
 # Create a function for binary search
-def binary_search(list, item):
-    low = 0
-    high = len(list) - 1
-
-    while low <= high:
-        mid = (low + high)
-        guess = list[mid]
-        if guess == item:
-            return mid
-        if guess > item:
-            high = mid - 1
-        else:
-            low = mid + 1
-    return None
